95c8b7f8a1255bfd6e4d4ce5d4b8aa591eb8165e
Daniil Viliavin <89505105005@mail.ru>
2020-12-11 17:02:36 +0300

    Fix configuration

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -1,9 +1,13 @@
 package com.example.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = ["com.example.*"])
 class BlogApplication
 
 fun main(args: Array<String>) {
